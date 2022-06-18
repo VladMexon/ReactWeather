8153d27b7b3de5650f0d1a975b5a7fe5a5a7612a
Vladislav Mekhonoshin <vladmexon@gmail.com>
2022-06-18 20:44:00 +0500

    ok

--- src/Components/HeaderComponent.tsx
+++ src/Components/HeaderComponent.tsx
@@ -2,7 +2,7 @@ export function Head()
 {
     return(
         <div className="Head">
-            <div id="headText">Погода в перми на 3 дня</div>
+            <div id="headText">Погода в Перми на 3 дня</div>
         </div>
     )
 }
